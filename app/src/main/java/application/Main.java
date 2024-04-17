5e752a078fa3e2a70403f276d5d5f8e2146e3c39
Lucas Adriano <lukasadriano94@hotmail.com>
2024-04-17 23:42:27 +0000

    aula17-04

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,25 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        int[] valores = new int [5];
+        
+        for(int cont =0;cont < valores.length;cont++){
+            try {
+                System.out.println("Digite um valor:");
+                Scanner sc = new Scanner (System.in);
+                valores[cont] = sc.nextInt();                
+            } catch (InputMismatchException exhn)  {
+                System.out.println("Digite um numero inteiro");
+                cont--;
+            }
+        }
+        System.out.println("Valores: ");
+        for (int val : valores) {
+            System.out.println(val + " ");
+        }
     }
 }
-
